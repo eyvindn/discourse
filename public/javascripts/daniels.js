0da8e2c0443c48961e3940ceb3274bd14b0c2fb5
dkijc <wwjdat2000@yahoo.com>
2017-11-17 13:33:55 -0500

    removed err

--- public/javascripts/daniels.js
+++ public/javascripts/daniels.js
@@ -48,23 +48,23 @@ var creationSuccess = function (data) {
 
   let postId = $('#post_1').attr("data-post-id");
 
-  $.get('http://discussion.dataforyouand.me/posts/' + postId + '.json?api_key=b5a802f239160697786c69a642e70ddaa7eafb78446f8f6cbcc78b11a2ba8e34&api_username=eyvind', function(err, result){
-    if(err) throw err;
+  $.get('http://discussion.dataforyouand.me/posts/' + postId + '.json?api_key=b5a802f239160697786c69a642e70ddaa7eafb78446f8f6cbcc78b11a2ba8e34&api_username=eyvind', function(result){
+    //if(err) console.log(err);
     
     let topic_id = result.topic_id;
     result.raw = 'Link to trello: ' + data.shortUrl + '\n' + result.raw;
 
-    $.put('http://discussion.dataforyouand.me/posts/' + postId + '.json?api_key=b5a802f239160697786c69a642e70ddaa7eafb78446f8f6cbcc78b11a2ba8e34&api_username=eyvind', result, function(err, afterPostsPutResult){
-      if(err) throw err;
+    $.put('http://discussion.dataforyouand.me/posts/' + postId + '.json?api_key=b5a802f239160697786c69a642e70ddaa7eafb78446f8f6cbcc78b11a2ba8e34&api_username=eyvind', result, function(afterPostsPutResult){
+      //if(err) console.log(err);
       
-      $.get('http://discussion.dataforyouand.me/t/-/' + topic_id + '.json?api_key=b5a802f239160697786c69a642e70ddaa7eafb78446f8f6cbcc78b11a2ba8e34&api_username=eyvind', function(err, topic){
-        if(err) throw err;
+      $.get('http://discussion.dataforyouand.me/t/-/' + topic_id + '.json?api_key=b5a802f239160697786c69a642e70ddaa7eafb78446f8f6cbcc78b11a2ba8e34&api_username=eyvind', function(topic){
+        //if(err) console.log(err);
 
         let params = {
           title: '[GRADUATED] ' + topic.title
         }
-        $.put('http://discussion.dataforyouand.me/t/' + topic.slug + '/' + topic.id + '.json?api_key=b5a802f239160697786c69a642e70ddaa7eafb78446f8f6cbcc78b11a2ba8e34&api_username=eyvind', params, function(err, ohgosh){
-          if(err) throw err;
+        $.put('http://discussion.dataforyouand.me/t/' + topic.slug + '/' + topic.id + '.json?api_key=b5a802f239160697786c69a642e70ddaa7eafb78446f8f6cbcc78b11a2ba8e34&api_username=eyvind', params, function(ohgosh){
+          //if(err) throw err;
           console.log('please work');
           $.get('http://discussion.dataforyouand.me');
         });
